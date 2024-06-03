bbca3949318f3ee6a2be0bb4a7d3769acf95c662
Jose Rios <roraj77@hotmail.com>
2024-06-03 02:27:51 -0400

    axios?2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api";
+const API_URL = "http://52.242.86.83:3000/api/"; 
 
 const instance = axios.create({
- 
   baseURL: API_URL,
   withCredentials: true,
 });
