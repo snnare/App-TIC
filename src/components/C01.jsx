f3ff34d22440a7f347076c3b3e59c56974db057b
Jose Rios <roraj77@hotmail.com>
2024-06-02 23:46:14 -0400

    SRC

---
+++ src/components/C01.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const C01 = () => {
+  return (
+    <View>
+      <Text>C01</Text>
+    </View>
+  )
+}
+
+export default C01
+
+const styles = StyleSheet.create({})
